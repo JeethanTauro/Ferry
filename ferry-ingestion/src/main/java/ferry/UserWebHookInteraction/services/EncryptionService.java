603b765b2268fc439ff983418f9a1b02799efff3
JeethanTauro <tauroshanjeeth@gmail.com>
2026-09-08 16:44:47 +0530

    feat(services): add services

---
+++ ferry-ingestion/src/main/java/ferry/UserWebHookInteraction/services/EncryptionService.java
@@ -0,0 +1,73 @@
+package ferry.UserWebHookInteraction.services;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Base64;
+
+@Service
+public class EncryptionService {
+
+    private final SecretKey secretKey;
+    private static final int IV_LENGTH = 12;
+
+    public EncryptionService(
+            @Value("${FERRY_ENCRYPTION_KEY}") String key) {
+
+        byte[] decodedKey = Base64.getDecoder().decode(key);
+        this.secretKey = new SecretKeySpec(decodedKey, "AES");
+    }
+
+    public String encrypt(String plainText) throws Exception {
+
+        byte[] iv = new byte[IV_LENGTH];
+        SecureRandom.getInstanceStrong().nextBytes(iv);
+
+        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+
+        GCMParameterSpec spec =
+                new GCMParameterSpec(128, iv);
+
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, spec);
+
+        byte[] encrypted =
+                cipher.doFinal(plainText.getBytes(StandardCharsets.UTF_8));
+
+        // Store IV + ciphertext together
+        byte[] result = new byte[iv.length + encrypted.length];
+
+        System.arraycopy(iv, 0, result, 0, iv.length);
+        System.arraycopy(encrypted, 0, result, iv.length, encrypted.length);
+
+        return Base64.getEncoder().encodeToString(result);
+    }
+
+    public String decrypt(String encryptedText) throws Exception {
+
+        byte[] combined =
+                Base64.getDecoder().decode(encryptedText);
+
+        byte[] iv = Arrays.copyOfRange(combined, 0, IV_LENGTH);
+        byte[] encrypted =
+                Arrays.copyOfRange(combined, IV_LENGTH, combined.length);
+
+        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+
+        GCMParameterSpec spec =
+                new GCMParameterSpec(128, iv);
+
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, spec);
+
+        byte[] decrypted = cipher.doFinal(encrypted);
+
+        return new String(decrypted, StandardCharsets.UTF_8);
+    }
+}
---
+++ ferry-ingestion/src/main/java/ferry/UserWebHookInteraction/services/WebhookService.java
@@ -0,0 +1,156 @@
+package ferry.UserWebHookInteraction.services;
+
+import ferry.UserWebHookInteraction.Util.FerryUtil;
+import ferry.UserWebHookInteraction.dtos.*;
+import ferry.UserWebHookInteraction.entities.WebhookEndpoint;
+import ferry.UserWebHookInteraction.entities.WebhookUsage;
+import ferry.UserWebHookInteraction.repos.WebhookEndpointRepo;
+import ferry.UserWebHookInteraction.repos.WebhookUsageRepo;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class WebhookService {
+    //create webhook : Done
+    //delete webhook : Done
+    //disable webhook : Done
+    //update webhook
+    //read a single webhook :Done
+    //read all the webhooks for a specific user (so that the dashboard can show the data) : Done
+
+    @Value("${FERRY_PUBLIC_URL}")
+    String baseUrl;
+
+    private final PasswordEncoder passwordEncoder;
+    private final WebhookEndpointRepo webhookEndpointRepo;
+    private final WebhookUsageRepo webhookUsageRepo;
+    private final EncryptionService encryptionService;
+
+
+    //creating the webhook and saving in the db
+    public WebhookEndpointCreatedResponse createWebhook(Long userId, String name, String destinationUrl) throws Exception {
+        //create a random unique id for the endpoint
+        //create a random secret
+        FerryUtil util = new FerryUtil();
+        String secretToken = util.generateSecureString(); //by default the len is 32
+        String uniqueId = util.generateSecureString(25);
+        WebhookEndpoint webhookEndpoint = WebhookEndpoint.builder()
+                .id(uniqueId)
+                .name(name)
+                .active(true)
+                .endpoint(baseUrl + "/webhooks/" + uniqueId)
+                .userId(userId)
+                .destinationUrl(destinationUrl)
+                .secret(encryptionService.encrypt(secretToken))
+                .createdAt(Instant.now())
+                .updatedAt(Instant.now())
+                .build();
+        webhookEndpointRepo.save(webhookEndpoint);
+
+        WebhookEndpointCreatedResponse response = WebhookEndpointCreatedResponse.builder()
+                .endpoint(webhookEndpoint.getEndpoint())
+                .secretToken(secretToken)
+                .build();
+
+        return response;
+        //return the dto as a response
+    }
+
+
+    //disable webhook
+    public void disableWebhook(String id, Long userId) {
+        WebhookEndpoint webhookEndpoint = webhookEndpointRepo
+                .findByIdAndUserId(id, userId)
+                .orElseThrow(() -> new RuntimeException("Webhook endpoint not found"));
+        webhookEndpoint.setActive(false);
+        webhookEndpoint.setUpdatedAt(Instant.now());
+        webhookEndpointRepo.save(webhookEndpoint);
+    }
+
+
+    //delete webhook (when the user click delete, we wil give a warning saying that this action cannot be redone)
+    public void deleteWebhook(String id, Long userId){
+        WebhookEndpoint webhookEndpoint = webhookEndpointRepo
+                .findByIdAndUserId(id, userId)
+                .orElseThrow(() -> new RuntimeException("Webhook endpoint not found"));
+        webhookEndpointRepo.delete(webhookEndpoint);
+        //return success
+    }
+
+
+    //read all the webhooks
+    public WebhooksResponse readWebhooks(Long userId){
+        //here we would have to do a join with the endpointUsage table so that when the list
+        //of the endpoints are on the dashboard we can see the usage
+        List<WebhookEndpoint> webhookEndpointList = webhookEndpointRepo.getAllByUserId(userId);
+        List<WebhookResponse> webhookResponseList = new ArrayList<>();
+
+        //creating the dto so we dont expose the secret hash
+        for(WebhookEndpoint wb : webhookEndpointList){
+            WebhookResponse webhookResponse = WebhookResponse.builder()
+                    .name(wb.getName())
+                    .endpoint(wb.getEndpoint())
+                    .createdAt(wb.getCreatedAt())
+                    .updatedAt(wb.getUpdatedAt())
+                    .destinationUrl(wb.getDestinationUrl())
+                    .rateLimit(wb.getRateLimitPerSecond())
+                    .active(wb.isActive())
+                    .build();
+            webhookResponseList.add(webhookResponse);
+        }
+
+        //list of all the web hooks
+        WebhooksResponse webhooksResponse = WebhooksResponse.builder()
+                .webhookResponseList(webhookResponseList)
+                .build();
+
+        return webhooksResponse;
+    }
+
+    public WebhookResponseWithUsage readWebhook(String id, Long userId) {
+
+        WebhookEndpoint endpoint = webhookEndpointRepo
+                .findByIdAndUserId(id, userId)
+                .orElseThrow(() -> new RuntimeException("Webhook endpoint not found"));
+
+        Optional<WebhookUsage> u = webhookUsageRepo.findByEndpointId(id);
+        WebhookUsage webhookUsage = new WebhookUsage();
+        if(u.isPresent()){
+            webhookUsage = u.get();
+        }
+        else{
+            webhookUsage = null;
+        }
+
+
+
+        WebhookResponse response = WebhookResponse.builder()
+                .name(endpoint.getName())
+                .endpoint(endpoint.getEndpoint())
+                .createdAt(endpoint.getCreatedAt())
+                .updatedAt(endpoint.getUpdatedAt())
+                .destinationUrl(endpoint.getDestinationUrl())
+                .rateLimit(endpoint.getRateLimitPerSecond())
+                .active(endpoint.isActive())
+                .build();
+
+        WebhookResponseUsage responseUsage = WebhookResponseUsage.builder()
+                .eventsReceived(webhookUsage != null ? webhookUsage.getEventsReceived() : 0L)
+                .eventsDelivered(webhookUsage != null ? webhookUsage.getEventsDelivered() : 0L)
+                .eventsFailed(webhookUsage != null ? webhookUsage.getEventsFailed() : 0L)
+                .build();
+
+        return WebhookResponseWithUsage.builder()
+                .webhookResponse(response)
+                .webhookResponseUsage(responseUsage)
+                .build();
+    }
+}
