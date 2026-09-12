61e9488e5800a167e7712d723943941a6621b03c
JeethanTauro <tauroshanjeeth@gmail.com>
2026-09-12 22:26:20 +0530

    fix(annotation): remove @builder and add no args and all args constructor

--- ferry-ingestion/src/main/java/ferry/UserWebHookInteraction/dtos/WebhookEndpointCreateRequest.java
+++ ferry-ingestion/src/main/java/ferry/UserWebHookInteraction/dtos/WebhookEndpointCreateRequest.java
@@ -4,9 +4,10 @@ package ferry.UserWebHookInteraction.dtos;
 import ferry.UserWebHookInteraction.entities.Provider;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@Builder
+@NoArgsConstructor
 public class WebhookEndpointCreateRequest {
     private String name;
     private String destinationUrl;
