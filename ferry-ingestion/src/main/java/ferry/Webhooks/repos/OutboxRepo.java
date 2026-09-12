19b856d8e2dfe91285bb84098c22d57441528ee3
JeethanTauro <tauroshanjeeth@gmail.com>
2026-09-12 22:24:12 +0530

    -

--- ferry-ingestion/src/main/java/ferry/Webhooks/repos/OutboxRepo.java
+++ ferry-ingestion/src/main/java/ferry/Webhooks/repos/OutboxRepo.java
@@ -1,7 +1,21 @@
 package ferry.Webhooks.repos;
 
 import ferry.Webhooks.entities.OutboxEvent;
+import ferry.Webhooks.entities.OutboxStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OutboxRepo extends JpaRepository<OutboxEvent, Long> {
+    @Query("""
+    SELECT o
+    FROM OutboxEvent o
+    JOIN FETCH o.webhookEvent
+    WHERE o.status = :status
+""")
+    List<OutboxEvent> findByStatusWithWebhookEvent(
+            @Param("status") OutboxStatus status
+    );
 }
