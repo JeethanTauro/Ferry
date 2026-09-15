852f09f1d0836ae3fa97704c210553336fab400f
JeethanTauro <tauroshanjeeth@gmail.com>
2026-09-15 19:54:48 +0530

    fix(dto): add @Data

--- ferry-ingestion/src/main/java/ferry/DeliveryToWorkers/dto/WebhookEventMessage.java
+++ ferry-ingestion/src/main/java/ferry/DeliveryToWorkers/dto/WebhookEventMessage.java
@@ -1,12 +1,16 @@
 package ferry.DeliveryToWorkers.dto;
 
 import lombok.Builder;
+import lombok.Data;
+
+import java.util.Map;
 
 @Builder
+@Data
 public class WebhookEventMessage {
     private Long eventId;
     private String endpointId;
     private String payload;
     private String destinationUrl;
-    private String headers;
+    private Map<String, String> headers;
 }
