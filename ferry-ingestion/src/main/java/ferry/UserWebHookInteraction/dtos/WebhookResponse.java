5f8721cc9a01b8ba9087fa22fa81980e0a43d447
JeethanTauro <tauroshanjeeth@gmail.com>
2026-09-24 19:35:54 +0530

    update(endpointId): add endpointId to response

--- ferry-ingestion/src/main/java/ferry/UserWebHookInteraction/dtos/WebhookResponse.java
+++ ferry-ingestion/src/main/java/ferry/UserWebHookInteraction/dtos/WebhookResponse.java
@@ -8,6 +8,7 @@ import java.time.Instant;
 @Data
 @Builder
 public class WebhookResponse {
+    private String endpointId;
     private String name;
     private String endpoint;
     private Instant createdAt;
