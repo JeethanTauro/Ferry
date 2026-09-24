a8d783791a4155e198ff499072e29e02e7518d77
JeethanTauro <tauroshanjeeth@gmail.com>
2026-09-24 19:34:55 +0530

    fix(cors): fix cors bug

--- ferry-ingestion/src/main/java/ferry/Auth/configs/SecurityConfig.java
+++ ferry-ingestion/src/main/java/ferry/Auth/configs/SecurityConfig.java
@@ -4,6 +4,7 @@ import ferry.Auth.security.FirebaseAuthenticationFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -36,6 +37,11 @@ public class SecurityConfig {
                         )
                 )
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                        HttpMethod.OPTIONS,
+                                        "/**"
+                                ).permitAll()
+
                         .requestMatchers(
                                 "/login",
                                 "/signup",
